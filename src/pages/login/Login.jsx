f56c83896b8001b98bbf0ec925966ca56c33d841
oxokaneoxo <kane.miller@hotmail.com>
2023-02-17 14:56:21 -0800

    Beginning of Social Media App

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,21 @@
+import './login.scss'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <div className='card'>
+        <div className='left'>
+          <h1>Hello, World!</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectertur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur
+          </p>
+        </div>
+        <div className='right'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
