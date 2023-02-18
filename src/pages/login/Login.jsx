4291effee8ba24a572b1833f48737b2b8158ee41
oxokaneoxo <kane.miller@hotmail.com>
2023-02-17 18:37:07 -0800

    Set up react router dom

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './login.scss'
 
 const Login = () => {
@@ -5,14 +6,25 @@ const Login = () => {
     <div className='login'>
       <div className='card'>
         <div className='left'>
-          <h1>Hello, World!</h1>
+          <h1>Hello World!</h1>
           <p>
             Lorem ipsum dolor sit amet consectertur adipisicing elit. Libero cum,
             alias totam numquam ipsa exercitationem dignissimos, error nam,
             consequatur
           </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
         </div>
-        <div className='right'></div>
+        <div className='right'>
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder='Username' />
+            <input type="password" placeholder='Password' />
+            <button>Login</button>
+          </form>
+        </div>
       </div>
     </div>
   )
